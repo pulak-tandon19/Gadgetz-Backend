bcf172797a77e6fd7857ed3b294511857f199baf
Pulak Tandon <pulaktandon2000@gmail.com>
2022-09-06 02:49:28 +0530

    Fourth Commit

--- backend/settings.py
+++ backend/settings.py
@@ -190,7 +190,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL= 'images/'
-MEDUA_ROOT= 'static/images'
+MEDIA_ROOT= 'static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
